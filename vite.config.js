04661ddb6af65cd38757c6ad76cd1dd68a119145
lucpergutUC3m <100560335@alumnos.uc3m.es>
2026-06-01 18:11:13 +0200

    fix: change in vite config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     // Proxy en desarrollo: las peticiones /api van al backend sin exponer la URL en el bundle
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://slmphish-tfm.swedencentral.cloudapp.azure.com:8443',
         changeOrigin: true,
         secure: false,
       },
